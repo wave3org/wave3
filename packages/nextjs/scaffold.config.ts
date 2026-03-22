78bf451b25562378405a78d3fd2ec2fda079ce8f
Victor Bravo <vbravo@fi.uba.ar>
2026-03-22 18:02:17 -0300

    Remove rcp overrides: api key preferred

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -26,7 +26,7 @@ const scaffoldConfig = {
 	// If you want to use a different RPC for a specific network, you can add it here.
 	// The key is the chain ID, and the value is the HTTP RPC URL
 	rpcOverrides: {
-		[chains.sepolia.id]: "https://sepolia.drpc.org"
+		// [chains.sepolia.id]: "https://rpc.sepolia.dev",
 	},
 	// This is ours WalletConnect's default project ID.
 	// You can get your own at https://cloud.walletconnect.com
