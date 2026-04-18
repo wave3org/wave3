b69ac526ee2417e247749c7cbcc08b1d2277db11
Federico Bogovic <fbogovic@fi.uba.ar>
2026-04-17 23:56:13 -0300

    feat: charge playback from the real listener account

--- packages/hardhat/contracts/Wave3SmartAccountFactory.sol
+++ packages/hardhat/contracts/Wave3SmartAccountFactory.sol
@@ -3,22 +3,31 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "./Wave3SmartAccount.sol";
 
+interface IWavecoinPlaybackAuthorizer {
+	function authorizePlaybackOperatorFor(address listener, address operator) external;
+}
+
 contract Wave3SmartAccountFactory {
 	mapping(address => address) private _accountOf;
+	IWavecoinPlaybackAuthorizer public immutable wavecoin;
 
 	event SmartAccountCreated(address indexed owner, address indexed account);
 
+	constructor(address wavecoinAddress) {
+		wavecoin = IWavecoinPlaybackAuthorizer(wavecoinAddress);
+	}
+
 	function createAccount(address owner) external returns (address account) {
 		account = _accountOf[owner];
-		if (account != address(0)) {
-			return account;
+		if (account == address(0)) {
+			Wave3SmartAccount smartAccount = new Wave3SmartAccount(owner);
+			account = address(smartAccount);
+			_accountOf[owner] = account;
+
+			emit SmartAccountCreated(owner, account);
 		}
 
-		Wave3SmartAccount smartAccount = new Wave3SmartAccount(owner);
-		account = address(smartAccount);
-		_accountOf[owner] = account;
-
-		emit SmartAccountCreated(owner, account);
+		wavecoin.authorizePlaybackOperatorFor(owner, account);
 	}
 
 	function getAccount(address owner) external view returns (address) {
